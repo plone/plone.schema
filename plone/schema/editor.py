c58eee00ff2338c64ee3431dfc1e5ab881ab2800
Franco Pellegrini <frapell@gmail.com>
2013-02-10 14:22:53 -0800

    Made the field to appear in the schemaeditor

--- plone/schema/editor.py
+++ plone/schema/editor.py
@@ -4,6 +4,11 @@ from zope.schema.interfaces import IURI
 
 from plone.schemaeditor.fields import FieldFactory
 
+from plone.schema.field import Email
+from plone.schema.interfaces import IEmail
+
+from plone.schema import _
+
 
 class IURI(IURI):
 
@@ -11,4 +16,11 @@ class IURI(IURI):
     default = Attribute('')
 
 
-URIFactory = FieldFactory(URI, u'URI Field')
+class IEmail(IEmail):
+
+    # prevent some settings from being included in the field edit form
+    default = Attribute('')
+
+
+URIFactory = FieldFactory(URI, _(u'URI Field'))
+EmailFactory = FieldFactory(Email, _(u'Email Field'))
