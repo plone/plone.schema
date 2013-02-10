0ef28d97a4f7916abd360c4fa5e3257f2f0e777d
Franco Pellegrini <frapell@gmail.com>
2013-02-10 14:20:11 -0800

    Don't actually need our own 'toUnicode'. Just using the default works. Also added handler for the email field

--- plone/schema/handler.py
+++ plone/schema/handler.py
@@ -5,33 +5,9 @@ except ImportError:
     HAVE_SUPERMODEL = False
 
 if HAVE_SUPERMODEL:
-
-    from zope.interface import implements
-    from zope.component import adapts
     from zope.schema import URI
-    from zope.schema.interfaces import IURI
-    from plone.supermodel.interfaces import IToUnicode
 
-    # class RichTextHandler_(BaseHandler):
-    #     """Special handling for the RichText field, to deal with 'default'
-    #     that may be unicode.
-    #     """
-
-    #     # Don't read or write 'schema'
-    #     filteredAttributes = BaseHandler.filteredAttributes.copy()
-    #     filteredAttributes.update({'schema': 'rw'})
-
-    #     def __init__(self, klass):
-    #         super(RichTextHandler_, self).__init__(klass)
-
-    class URIToUnicode(object):
-        implements(IToUnicode)
-        adapts(IURI)
-
-        def __init__(self, context):
-            self.context = context
-
-        def toUnicode(self, value):
-            return value.raw
+    from plone.schema.field import Email
 
     URIHandler = BaseHandler(URI)
+    EmailHandler = BaseHandler(Email)
