0703240d5c51af189d8261040bab59dc4b151c1a
Ian Anderson <ianderso@med.unc.edu>
2014-03-16 12:29:22 -0700

    moved from IFormLayer to IPloneFormLayer

--- plone/schema/browser/email.py
+++ plone/schema/browser/email.py
@@ -1,11 +1,13 @@
 from zope.component import adapter
 from zope.interface import implementer
 from z3c.form.interfaces import ITextWidget
-from z3c.form.interfaces import IFormLayer
 from z3c.form.interfaces import IFieldWidget
 from z3c.form.browser.text import TextWidget
 from z3c.form.widget import FieldWidget
-from plone.schema.interfaces import IEmail
+
+from plone.app.z3cform.interfaces import IPloneFormLayer
+
+from plone.schema.email import IEmail
 
 
 class IEmailWidget(ITextWidget):
@@ -18,7 +20,7 @@ class EmailWidget(TextWidget):
     value = None
 
 
-@adapter(IEmail, IFormLayer)
+@adapter(IEmail, IPloneFormLayer)
 @implementer(IFieldWidget)
 def EmailFieldWidget(field, request):
     return FieldWidget(field, EmailWidget(request))
--- plone/schema/browser/uri.py
+++ plone/schema/browser/uri.py
@@ -1,12 +1,12 @@
 from z3c.form.browser.text import TextWidget
 from z3c.form.interfaces import IFieldWidget
-from z3c.form.interfaces import IFormLayer
 from z3c.form.interfaces import ITextWidget
 from z3c.form.widget import FieldWidget
 from zope.component import adapter
 from zope.interface import implementer
 from zope.schema.interfaces import IURI
 
+from plone.app.z3cform.interfaces import IPloneFormLayer
 
 class IURIWidget(ITextWidget):
     """ URI Widget """
@@ -18,7 +18,7 @@ class URIWidget(TextWidget):
     value = None
 
 
-@adapter(IURI, IFormLayer)
+@adapter(IURI, IPloneFormLayer)
 @implementer(IFieldWidget)
 def URIFieldWidget(field, request):
     return FieldWidget(field, URIWidget(request))
