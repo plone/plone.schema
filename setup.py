a05251c6dad5e1a27264c8dd253df16e99684ec8
Gil Forcada <gforcada@gnome.org>
2014-11-17 00:51:50 +0100

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -2,33 +2,36 @@ from setuptools import setup, find_packages
 
 version = '1.0a2.dev0'
 
-setup(name='plone.schema',
-      version=version,
-      description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
-          "Framework :: Zope2",
-          "Framework :: Plone",
-          "Programming Language :: Python",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "License :: OSI Approved :: BSD License",
-      ],
-      keywords='plone schema',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://plone.org/',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.dexterity',
-          'plone.app.z3cform'
-      ],
-      extras_require={
-          'test': ['plone.app.testing'],
-      },
-      )
+long_description = open("README.rst").read() + "\n" +
+                   open("CHANGES.rst").read()
+
+setup(
+    name='plone.schema',
+    version=version,
+    description="",
+    long_description=long_description,
+    classifiers=[
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords='plone schema',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://plone.org/',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.dexterity',
+        'plone.app.z3cform'
+    ],
+    extras_require={
+        'test': ['plone.app.testing'],
+    },
+)
